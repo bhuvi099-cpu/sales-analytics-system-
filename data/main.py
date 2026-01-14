2cf8ae0bf5cf4750db869bde9fe2b341f6409b01
bhuvi099-cpu <bhuvi099@gmail.com>
2026-01-14 20:41:19 +0530

    Update main.py

--- data/main.py
+++ data/main.py
@@ -1,19 +1,12 @@
-from utils.file_handler import read_sales_file, write_clean_data
-from utils.data_processor import clean_sales_data
-from utils.api_handler import fetch_product_info
-
-DATA_FILE = "data/sales_data.txt"
-OUTPUT_FILE = "output/clean_sales_data.csv"
-
-def main():
-    raw_data = read_sales_file(DATA_FILE)
-    cleaned_data = clean_sales_data(raw_data)
-
-    write_clean_data(OUTPUT_FILE, cleaned_data)
-
-    # Example API call
-    product_info = fetch_product_info("Laptop")
-    print("Sample API Data:", product_info)
-
-if __name__ == "__main__":
-    main()
+PS C:\Users\punam\OneDrive\Documents\Pandasapp\assignment> ^C
+PS C:\Users\punam\OneDrive\Documents\Pandasapp\assignment> cd "c:\Users\punam\OneDrive\Documents\Pandasapp\assignment\sales-analytics-system"; python main.py
+Cleaned data saved to C:\Users\punam\OneDrive\Documents\Pandasapp\assignment\sales-analytics-system\output\cleaned.csv
+  TransactionID        Date ProductID     ProductName  ...  UnitPrice  CustomerID Region TotalPrice
+0          T018  2024-12-29      P107       USB Cable  ...      173.0        C009  South     1384.0
+1          T063  2024-12-07      P110  Laptop Charger  ...     1916.0        C022   East    11496.0
+2          T075  2024-12-10      P106      Headphones  ...     2826.0        C001  South        0.0
+3          T023  2024-12-09      P109  Wireless Mouse  ...      523.0        C022  North     4707.0
+4          T059  2024-12-29      P102  Mouse Wireless  ...     1056.0        C010  South     4224.0
+ 
+[5 rows x 9 columns]
+PS C:\Users\punam\OneDrive\Documents\Pandasapp\assignment\sales-analytics-system>
